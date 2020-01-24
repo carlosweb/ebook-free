6890b496c3c53e90dc51f39eb06d5867f6c1d54a
carlosweb <kaduinfo@gmail.com>
2020-01-24 13:06:09 -0300

    first commit

---
+++ js/ebook.js
@@ -0,0 +1,22 @@
+function registrar() {
+    let emailTxt = document.getElementById('email').value
+    let res = document.getElementById('res')
+    let timeebook = new Date().getTime()
+
+    var database = firebase.database()
+    database.ref(timeebook).set({
+        email: emailTxt
+    })
+
+    // if(emailTxt == 0) {
+    //     res.innerHTML = `Preencha o campo do e-mail`
+    //     res.style.display = 'block'
+    //     res.classList.add('alert-danger')
+    // } else {
+    //     res.innerHTML = `Seu pedido foi enviado com sucesso!`
+    //     res.style.display = 'block'
+    //     res.classList.remove('alert-danger')
+    //     res.classList.add('alert-success')
+    // }
+
+}
---
+++ js/init.js
@@ -0,0 +1,12 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyBZbuoWsPGP0j7zi1y7SPbGTLRuJyDWuUM",
+    authDomain: "ebookfotografia.firebaseapp.com",
+    databaseURL: "https://ebookfotografia.firebaseio.com",
+    projectId: "ebookfotografia",
+    storageBucket: "ebookfotografia.appspot.com",
+    messagingSenderId: "503759344480",
+    appId: "1:503759344480:web:06629c3296b276c6fd8405"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
